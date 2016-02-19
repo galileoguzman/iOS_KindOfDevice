bf93f97d92261c0f932424a7e5c708cbb41b30d2
Galileo Guzmán <galileoguzman@gmail.com>
2016-02-19 12:18:51 -0600

    Check for kind of device

--- KindDevice/ViewController.m
+++ KindDevice/ViewController.m
@@ -32,7 +32,19 @@
 -(void)initController{
     
     
-    
+    // Check for device model
+    if ([SDiOSVersion deviceVersion] == iPhone6S)
+        NSLog(@"You got the iPhone 6S. Sweet 🍭!");
+    else if ([SDiOSVersion deviceVersion] == iPhone6SPlus)
+        NSLog(@"iPhone 6 S Plus? Bigger is better!");
+    else if ([SDiOSVersion deviceVersion] == iPhone6Plus)
+        NSLog(@"iPhone 6 Plus? Bigger is better!");
+    else if ([SDiOSVersion deviceVersion] == iPadPro)
+        NSLog(@"You own an iPad Pro 🌀!");
+    else if ([SDiOSVersion deviceVersion] == iPhone5S)
+        NSLog(@"You own an iPhone 5s");
+    else if ([SDiOSVersion deviceVersion] == iPhone5)
+        NSLog(@"You own an iPhone 5");
     
 }
 
